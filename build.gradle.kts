031babc62bdd2be9dc0703030fff0366585cfa60
Sakura Sedaia <email@sakura-sedaia.com>
2026-09-15 08:38:18 -0500

    [Build: solidjs]: Integrate the frontend build with Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,3 +27,48 @@ dependencies {
   testImplementation(kotlin("test"))
   testImplementation(ktorLibs.server.testHost)
 }
+
+val solidJsDirectory = layout.projectDirectory.dir("src/main/solidjs")
+val solidJsOutput = solidJsDirectory.dir("dist/client")
+
+tasks.processResources {
+  dependsOn(buildSolidJs)
+  
+  from(solidJsOutput) {
+    into("static")
+  }
+}
+
+
+val pnpmInstall by tasks.registering(Exec::class) {
+  description = "Installs the SolidJS Dependencies"
+  group = "build"
+  
+  workingDir(solidJsDirectory)
+  environment("CI", "true")
+  commandLine("/Users/Sakura/Library/pnpm/pnpm", "install", "--frozen-lockfile")
+  
+  inputs.files(
+    solidJsDirectory.file("package.json"),
+    solidJsDirectory.file("pnpm-lock.yaml"),
+    solidJsDirectory.file("pnpm-workspace.yaml"),
+  )
+  outputs.dir(solidJsDirectory.dir("node_modules"))
+}
+
+val buildSolidJs by tasks.registering(Exec::class) {
+  description = "Builds the SolidJS frontend"
+  group = "build"
+  
+  dependsOn(pnpmInstall)
+  workingDir(solidJsDirectory)
+  environment("CI", "true")
+  commandLine("/Users/Sakura/Library/pnpm/pnpm", "build")
+  
+  inputs.files(
+    fileTree(solidJsDirectory) {
+      exclude("node_modules/**", "dist/**")
+    }
+  )
+  outputs.dir(solidJsOutput)
+}
