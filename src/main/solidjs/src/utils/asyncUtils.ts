f144cb3f1ce2de621f1c8940befa48e392145e03
SakuraSedaia <email@sakura-sedaia.com>
2026-09-15 11:19:24 -0500

    [Add: solidjs]: Additional Diagnostic logs added

--- src/main/solidjs/src/utils/asyncUtils.ts
+++ src/main/solidjs/src/utils/asyncUtils.ts
@@ -45,7 +45,13 @@ interface AsyncFetchProps {
  * ```
  */
 export async function asyncFetch<DataType>({apiRoute, errorMessage = "Failed to fetch data",}: AsyncFetchProps): Promise<DataType> {
-  const res = await fetch(`${apiRoot}/${apiRoute}`)
+  const parsedRoute = `${apiRoot}/${apiRoute}`
+  console.log(`Sending request to ${parsedRoute}`)
+
+  const res = await fetch(parsedRoute)
+
+  console.log(res.status, res.headers.get("content-type"));
+  console.log(`Received: ${await res.clone().text()}`);
 
   if (!res.ok) throw new Error(`${errorMessage}: ${res.status}`)
 
